2452c8fe619d0eab7a192af4acdc7136873782dd
Behrouze <dev@behrouze.com>
2020-11-01 22:46:56 +0100

    with collapsible col

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-document.querySelector('.js-button').addEventListener('click', function () {
-    console.log('test')
-    document.querySelector('.js-grid').classList.toggle('grid--full')
-})
+document.querySelector('.js-button')
+    .addEventListener('click', function () {
+        document.querySelector('.js-grid')
+            .classList.toggle('grid--full')
+    })
